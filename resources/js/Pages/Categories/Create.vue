b5cedce9773c06dfa9e6d649bbc264411a953194
Eduardo Lazaro III <69066788+emlazaroiii@users.noreply.github.com>
2021-04-21 09:23:34 +0800

    Code Refactor

--- resources/js/Pages/Categories/Create.vue
+++ resources/js/Pages/Categories/Create.vue
@@ -100,6 +100,8 @@ export default {
         { id: 2, name: "2" },
         { id: 3, name: "3" },
         { id: 4, name: "4" },
+        { id: 5, name: "5" },
+        { id: 6, name: "6" },
       ],
       form: this.$inertia.form({
         name: null,
