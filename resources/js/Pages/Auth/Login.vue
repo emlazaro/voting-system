6f43aa58a1062350cdfbbfb02a18cd5bf3da4275
Eduardo Lazaro III <69066788+emlazaroiii@users.noreply.github.com>
2021-04-26 15:35:49 +0800

    code refactor

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,104 +1,107 @@
 <template>
-  <jet-authentication-card
-    class="flex flex-col items-center justify-center min-h-screen p-4"
-  >
-    <div class="fixed top-0 right-0 hidden px-6 py-4 sm:block">
-      <inertia-link
-        v-if="$page.props.user"
-        href="/dashboard"
-        class="text-sm text-gray-700 underline"
-      >
-        Dashboard
-      </inertia-link>
-
-      <template v-else>
+  <div id="login-box">
+    <jet-authentication-card
+      class="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-50 bg-opacity-60"
+    >
+      <div class="fixed top-0 right-0 hidden px-6 py-4 sm:block">
         <inertia-link
-          v-if="canRegister"
-          :href="route('register')"
-          class="ml-4 text-sm text-gray-700 underline"
+          v-if="$page.props.user"
+          href="/dashboard"
+          class="text-sm text-gray-700 underline"
         >
-          Register
+          Dashboard
         </inertia-link>
+
+        <template v-else>
+          <inertia-link
+            v-if="canRegister"
+            :href="route('register')"
+            class="ml-4 text-sm text-gray-700 underline"
+          >
+            Register
+          </inertia-link>
+        </template>
+      </div>
+      <template #logo>
+        <div class="flex items-center space-x-4">
+          <jet-authentication-card-logo class="text-gray-800" />
+
+          <div class="text-5xl text-gray-800">Voting System</div>
+        </div>
       </template>
-    </div>
-    <template #logo>
-      <div class="flex items-center space-x-4">
-        <jet-authentication-card-logo />
+      <jet-validation-errors class="mb-4" />
 
-        <div class="text-4xl text-gray-600">Voting System</div>
-      </div>
-    </template>
-    <jet-validation-errors class="mb-4" />
-
-    <div v-if="status" class="mb-4 text-sm font-medium text-green-600">
-      {{ status }}
-    </div>
-
-    <form @submit.prevent="submit">
-      <div>
-        <jet-label for="email" value="Email" />
-        <jet-input
-          id="email"
-          type="email"
-          class="block w-full mt-1"
-          v-model="form.email"
-          required
-          autofocus
-        />
+      <div v-if="status" class="mb-4 text-sm font-medium text-green-600">
+        {{ status }}
       </div>
 
-      <div class="mt-4">
-        <jet-label for="password" value="Password" />
-        <jet-input
-          id="password"
-          type="password"
-          class="block w-full mt-1"
-          v-model="form.password"
-          required
-          autocomplete="current-password"
-        />
-      </div>
+      <form @submit.prevent="submit">
+        <div>
+          <jet-label for="email" value="Email" />
+          <jet-input
+            id="email"
+            type="email"
+            class="block w-full mt-1"
+            v-model="form.email"
+            required
+            autofocus
+          />
+        </div>
 
-      <div class="flex justify-between mt-4">
-        <label class="flex items-center">
-          <jet-checkbox name="remember" v-model:checked="form.remember" />
-          <span class="ml-2 text-sm text-gray-600">Remember me</span>
-        </label>
-        <inertia-link
-          v-if="canResetPassword"
-          :href="route('password.request')"
-          class="text-sm text-gray-600 underline hover:text-gray-900"
-        >
-          Forgot your password?
-        </inertia-link>
-      </div>
+        <div class="mt-4">
+          <jet-label for="password" value="Password" />
+          <jet-input
+            id="password"
+            type="password"
+            class="block w-full mt-1"
+            v-model="form.password"
+            required
+            autocomplete="current-password"
+          />
+        </div>
 
-      <div class="flex justify-between mt-4 space-x-3">
-        <inertia-link
-          as="button"
-          v-if="canRegister"
-          :href="route('register')"
-          class="w-1/2 text-sm text-center text-gray-700 btn btn-outline-success"
-        >
-          Register
-        </inertia-link>
-        <button
-          class="w-1/2 btn btn-primary"
-          :class="{ 'opacity-25': form.processing }"
-          :disabled="form.processing"
-        >
-          Log in
-        </button>
-        <!-- <jet-button
+        <div class="flex justify-between mt-4">
+          <label class="flex items-center">
+            <jet-checkbox name="remember" v-model:checked="form.remember" />
+            <span class="ml-2 text-sm text-gray-600">Remember me</span>
+          </label>
+          <inertia-link
+            v-if="canResetPassword"
+            :href="route('password.request')"
+            class="text-sm text-gray-600 underline hover:text-gray-900"
+          >
+            Forgot your password?
+          </inertia-link>
+        </div>
+
+        <div class="flex justify-between mt-4 space-x-3">
+          <button
+            class="w-1/2 btn btn-primary"
+            :class="{ 'opacity-25': form.processing }"
+            :disabled="form.processing"
+          >
+            Log in
+          </button>
+          <!-- <jet-button
           class="ml-2"
           :class="{ 'opacity-25': form.processing }"
           :disabled="form.processing"
         >
           Log in
         </jet-button> -->
-      </div>
-    </form>
-  </jet-authentication-card>
+
+          <inertia-link
+            as="button"
+            v-if="canRegister"
+            :href="route('register')"
+            class="w-1/2 text-sm text-center text-gray-700 btn btn-outline-success"
+          >
+            Register
+          </inertia-link>
+        </div>
+      </form>
+    </jet-authentication-card>
+  </div>
 </template>
 
 <script>
